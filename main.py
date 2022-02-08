0b2216d4f27256de78ce91f466c5ab7b9cef2d89
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2022-02-08 18:28:27 +0100

    Added Info ELO seection
    Aggiunta sezione con informazioni riguardanti le formule per calcolare l'ELO

--- main.py
+++ main.py
@@ -723,19 +723,33 @@ if pagina_selezionata == "Statistiche mazzo 📈":
 # PAGINA: "Info ELO"
 if pagina_selezionata == "Info ELO":
 
-    matches, lista_mazzi, tournaments = download_data()
+    st.markdown("## ELO-system")
+    st.markdown("Fonte: 🌍 [link](https://metinmediamath.wordpress.com/2013/11/27/how-to-calculate-the-elo-rating-including-example/)")
 
-    Mazzi = st.expander("Mazzi 👉")
-    with Mazzi:
-        lista_mazzi_info = lista_mazzi.copy()
-        lista_mazzi_info.set_index('owner', inplace=True)
-        st.write(lista_mazzi_info[["deck_name","deck_category"]])
+    st.latex(r'''
+        \text{Expected score player 1} = E_1 = \frac{R_1}{(R_1+R_2)}
+        ''')
+    st.markdown("where: ")
 
-    Duelli = st.expander("Duelli 👉")
-    with Duelli:
-        st.write(matches)
+    st.latex(r'''
+        R_n = 10^{r_n/400}
+    ''')
+    st.latex(r'''
+        r_n: \text{score of player}\  n
+    ''')
 
+    st.markdown("After the match is finished, the actual score is set:")
+    st.latex(r'''
+        S_1 = 1\ \text{if player 1 wins}, 0.5\ \text{if draw}, 0\ \text{if player 2 wins} 
+    ''')
+    st.latex(r'''
+        S_2 = 0\ \text{if player 1 wins}, 0.5\ \text{if draw}, 1\ \text{if player 2 wins} 
+    ''')
 
+    st.markdown("In the last step, putting all together, for each player the updated Elo-rating is computed:")
+    st.latex(r'''
+        r^{'}_n=r_n+K(S_n-E_n)
+    ''')
 
 
 
